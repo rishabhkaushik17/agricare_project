8a7bab2ee4ac8d80a596d399b02d19a9b3c7e2b6
Rishabh <kaushikrishabh24@gmail.com>
2023-01-26 23:45:34 +0530

    Added fertilizer_prediction.py and modified urls.py and views.py

--- buysell/urls.py
+++ buysell/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('home/sell/', views.display),
     path('home/recommend', views.get_recommendation),
     path('home/recommend/result', views.get_crop_result, name='result'),
+    path('home/predict', views.get_prediction),
+    path('home/predict/answer', views.get_fertilizer_result, name='answer'),
 ]
